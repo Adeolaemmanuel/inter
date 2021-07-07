6ea247d65dd8eb66a3051c3702e739cb630bca76
Adeola Emmanuel <adeolaemmanuel006@gmail.com>
2021-07-07 12:05:06 +0100

    update

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="w3-bar w3-padding-16">
+    <nav className="w3-bar w3-padding-16 w3-top w3-white">
       <div className="w3-bar-item w3-btn w3-black w3-round w3-margin-left">
         <Link href="/">Home</Link>
       </div>
